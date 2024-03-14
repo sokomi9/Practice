ea6f0734d259d36f3af6a5075a62d4e3b1179add
abiudsokomi@gmail.com <abiudsokomi@gmail.com>
2024-03-14 13:03:55 +0300

    my commit

---
+++ Vehicles.java
@@ -0,0 +1,30 @@
+public class Vehicles {
+    
+    String name;
+    int price;
+    
+    public Vehicles(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String toString() {
+        return name + " : " + price;
+    }
+    
+    // public String getName() {
+    //     return name;
+    // }
+    // public void setName(String name) {
+    //     this.name = name;
+    // }
+    // public int getPrice() {
+    //     return price;
+    // }
+    // public void setPrice(int price) {
+    //     this.price = price;
+    // }
+       
+
+
+}
---
+++ index.java
@@ -0,0 +1,12 @@
+
+public class index {
+
+    public static void main(String[] args) {
+        Vehicles vehicle1 = new Vehicles("Lamborghini", 100000);
+        Vehicles vehicle2 = new Vehicles("Escalade", 90000);
+        //vehicles.setName("Lamborghini");
+        //vehicles.setPrice(1000000);
+        System.out.println(vehicle1);
+        System.out.println(vehicle2);
+    }
+}
